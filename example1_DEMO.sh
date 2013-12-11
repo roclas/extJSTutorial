7da8f7ba6b3755a1555ffe2548e93426f2d703a2
carlos <carlos@beeva.(none)>
2013-12-11 20:28:17 +0100

    minor changes

--- example1_DEMO.sh
+++ example1_DEMO.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 wget http://cdn.sencha.io/ext-4.0.7-gpl.zip
-unzip ext*gpl.zip -d example0/
-cd example0
+unzip ext*gpl.zip -d example1/
+cd example1
 python -m SimpleHTTPServer 
-firefox http://localhost:8000/examples/desktop/desktop.html
+firefox http://localhost:8000/ext-4.0.7-gpl/examples/desktop/desktop.html
 cd -
+
